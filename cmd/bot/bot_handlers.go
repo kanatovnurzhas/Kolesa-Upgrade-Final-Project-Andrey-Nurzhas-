ee50489f29d19362c9f28f4ac0fb21f8b985b905
Nurzhas <nurzhaskaantovbarca@gmail.com>
2022-11-16 23:26:15 +0600

    добавил обработчик ПОСТ запроса и чуть чуть по папкам распределил

---
+++ cmd/bot/bot_handlers.go
@@ -0,0 +1,37 @@
+package tgbot
+
+import (
+	"gobot/internal/models"
+	"gopkg.in/telebot.v3"
+	"log"
+)
+
+func (bot *UpgradeBot) StartHandler(ctx telebot.Context) error {
+	newUser := models.User{
+		Name:       ctx.Sender().Username,
+		TelegramId: ctx.Chat().ID,
+		FirstName:  ctx.Sender().FirstName,
+		LastName:   ctx.Sender().LastName,
+		ChatId:     ctx.Chat().ID,
+	}
+
+	existUser, err := bot.Users.FindOne(ctx.Chat().ID)
+
+	if err != nil {
+		log.Printf("Ошибка получения пользователя %v", err)
+	}
+
+	if existUser == nil && err == nil {
+		err := bot.Users.Create(newUser)
+
+		if err != nil {
+			log.Printf("Ошибка создания пользователя %v", err)
+		}
+	}
+
+	return ctx.Send("Привет, " + ctx.Sender().FirstName)
+}
+
+func SendMessage() {
+
+}
--- main.go
+++ main.go
@@ -1,57 +1,12 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/BurntSushi/toml"
 	"gobot/cmd/bot"
-	"gobot/internal/models"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"log"
-	"net/http"
+	"gobot/cmd/server"
 )
 
-type Config struct {
-	Env      string
-	BotToken string
-	Dsn      string
-}
-type Recipient interface {
-	// Must return legit Telegram chat_id or username
-	Recipient() string
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-	configPath := flag.String("config", "", "Path to config file")
-	flag.Parse()
+	server.Server()
+	tgbot.RunBot()
 
-	cfg := &Config{}
-	_, err := toml.DecodeFile(*configPath, cfg)
-
-	if err != nil {
-		log.Fatalf("Ошибка декодирования файла конфигов %v", err)
-	}
-
-	db, err := gorm.Open(mysql.Open(cfg.Dsn), &gorm.Config{})
-
-	if err != nil {
-		log.Fatalf("Ошибка подключения к БД %v", err)
-	}
-
-	upgradeBot := bot_init.UpgradeBot{
-		Bot:   bot_init.InitBot(cfg.BotToken),
-		Users: &models.UserModel{Db: db},
-	}
-
-	upgradeBot.Bot.Handle("/start", upgradeBot.StartHandler)
-	upgradeBot.Bot.Start()
-}
-func handler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	x := r.Form.Get("text")
-	fmt.Fprint(w, x)
-	//Я сделал вывод в консоль пост запроса с нашим сообщением, осталось вывести её в переменную и разослать всем с помощью метода telebot.bot.Send()
 }
